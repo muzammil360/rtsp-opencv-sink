a94268bda818f3fcd168033b7554455e6cf169f3
muzammil360 <muzammil360@gmail.com>
2020-12-20 10:52:23 +0500

    added frame count

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def print_time():
 
 def main(opts):
     address = opts.address
-    delay = opts.delay 
+    delay = opts.delay
+    n_frames = opts.n_frames
     
     vcap = cv2.VideoCapture(address, cv2.CAP_FFMPEG)
-    while(1):
+    for i in range(n_frames):
+        print('reading frame no. : {}'.format(i))
         ret, frame = vcap.read()
         if ret is False:
             print("Frame is empty")
@@ -27,6 +29,7 @@ def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('--address', type=str, default='http://107.3.49.194:1024/mjpg/video.mjpg', help='camera stream address')
     parser.add_argument('--delay', type=int, default = 1000,  help='delay in milliseconds b/w two frames')
+    parser.add_argument('--n_frames', type=int, default = 10,  help='no. of frames to read before exit')
     
     args = parser.parse_args()
     print(args)
